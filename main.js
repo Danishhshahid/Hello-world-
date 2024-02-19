925148eeb0ead5d66ca944121e0a463ec2f7b1c6
Danish Shahid Abbasi <160201946+Danishhshahid@users.noreply.github.com>
2024-02-19 10:13:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//method 1
+var sms = "i am danish Abbasi";
+console.log(sms);
+//method 2
+console.log("hello there! my name is danish abbasi");
---
+++ main.ts
@@ -0,0 +1,8 @@
+//method 1
+let sms = "i am danish Abbasi";
+console.log(sms);
+
+
+
+//method 2
+console.log("hello there! my name is danish abbasi")
